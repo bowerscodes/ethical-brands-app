2d068d1b82451d32abf8da0d9f2f8675bfdc0f73
Thomas <thomas.bogle@outlook.com>
2022-07-10 15:14:03 +0100

    Added console.log of data in App and getBrand axios request

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
+import axios from "axios";
 import Search from "./components/Search";
 import SearchResult from "./components/SearchResult";
+
+const getData = async () => {
+  try {
+    await axios.get("http://localhost:3000/brands").then((res) => {
+      console.log(res.data);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getData();
 
 const App = () => {
   return (
