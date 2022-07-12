98374f49d5ce3dfb95113215bb2cfe79799c6cdb
StuartCowley <voidstalker@live.co.uk>
2022-07-12 21:26:56 +0100

    Fixes for get request

--- src/requests/getByName.js
+++ src/requests/getByName.js
@@ -1,53 +1,45 @@
 import axios from "axios";
 
-const getByName = async (
-  searchText,
-  setLogo,
-  setName,
-  setCategory,
-  setOwnership,
-  setRevenue,
-  setAnTesting,
-  setAnInfo,
-  setEnvScore,
-  setEnvInfo,
-  setLabScore,
-  setLabInfo,
-  setPhilDonate,
-  setPhilInfo,
-  setNewsSource,
-  setNewsHeadline,
-  setTotalScore
-) => {
+const getByName = async (searchText, setResults, setErrorMessage) => {
   let endpoint = "http://localhost:3000/brands";
 
   if (searchText) {
     endpoint += `/name/${searchText}`;
   }
+  setErrorMessage("");
 
-  try {
-    await axios.get(endpoint).then((res) => {
-      console.log(res.data);
-      setLogo(res.data.logo);
-      setName(res.data.name);
-      setCategory(res.data.category);
-      setRevenue(res.data.revenue);
-      setOwnership(res.data.ownership.owner);
-      setAnTesting(res.data.animals.testing);
-      setAnInfo(res.data.animals.info);
-      setEnvScore(res.data.environment.score);
-      setEnvInfo(res.data.environment.info);
-      setLabScore(res.data.labour.score);
-      setLabInfo(res.data.labour.info);
-      setPhilDonate(res.data.philanthropy.donations);
-      setPhilInfo(res.data.philanthropy.info);
-      setNewsSource(res.data.news[0].source);
-      setNewsHeadline(res.data.news[0].headline);
-      setTotalScore(res.data.totalScore);
+  axios
+    .get(endpoint)
+    .then((res) => {
+      if (res.data === null) {
+        setErrorMessage("Invalid brand");
+      } else {
+        setErrorMessage("");
+        const returned = {
+          logo: res.data.logo,
+          name: res.data.name,
+          category: res.data.category,
+          ownership: res.data.ownership.owner,
+          revenue: res.data.revenue,
+          anTesting: res.data.animals.testing,
+          anInfo: res.data.animals.info,
+          envScore: res.data.environment.score,
+          envInfo: res.data.environment.info,
+          labScore: res.data.labour.score,
+          labInfo: res.data.labour.info,
+          philDonate: res.data.philanthropy.donations,
+          philInfo: res.data.philanthropy.info,
+          newsSource: res.data.news[0].source,
+          newsHeadline: res.data.news[0].headline,
+          totalScore: res.data.totalScore,
+        };
+        setResults(returned);
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      setErrorMessage("server error!");
     });
-  } catch (error) {
-    console.log(error);
-  }
 };
 
 export default getByName;
