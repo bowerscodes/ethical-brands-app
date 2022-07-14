3c25b447ddd40c8a55d7e76fb89e81ac6bb08311
James Bowers <jamesbowers123@gmail.com>
2022-07-14 19:51:42 +0100

    reformat to match figma design

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,31 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/search.css";
-import Alert from "./Alert";
 
 // eslint-disable-next-line react/prop-types
 const Search = ({ searchText, setSearchText, onSubmit }) => {
   const handleInputChange = (event) => setSearchText(event.target.value);
 
   return (
-    <div className="search-component">
-      <Alert message="Not in database" success={false} />
-      <h2 className="search-title">Ethical Brands</h2>
-      <form className="search-form">
-        <div>
-          <input
-            type="text"
-            className="search-input"
-            onChange={handleInputChange}
-            value={searchText}
-          />
+    <div className="search-page">
+      <div className="search-background">
+        <div className="search-component">
+          <h2 className="search-title">Ethical Brands</h2>
+          <form className="search-form">
+            <div>
+              <input
+                type="text"
+                className="search-input"
+                onChange={handleInputChange}
+                value={searchText}
+              />
+            </div>
+            <div>
+              <button
+                type="submit"
+                className="search-button"
+                onClick={onSubmit}
+              >
+                <Link to="/search-results">SEARCH</Link>
+              </button>
+            </div>
+          </form>
         </div>
-        <div>
-          <button type="submit" className="search-button" onClick={onSubmit}>
-            <Link to="/search-results">SEARCH</Link>
-          </button>
-        </div>
-      </form>
+      </div>
     </div>
   );
 };
